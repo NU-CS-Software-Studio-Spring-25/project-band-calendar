fb566f3fdd3184e0700d9e194d20b4820b63522d
Kevin Wang <kenwang3333@gmail.com>
2025-04-30 13:40:36 -0500

    feat: create new event form and routes, styled with bootstrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,4 +11,19 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :venue, :location, :date)
+  end
 end
