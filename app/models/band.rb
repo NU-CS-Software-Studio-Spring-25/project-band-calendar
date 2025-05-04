8093e853f0ee5f66ae1d6131537d3ac77ec53d44
Kevin Wang <kenwang3333@gmail.com>
2025-05-04 10:21:07 -0500

    Add validations to Band and Event models to prevent duplicates

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,3 +1,5 @@
 class Band < ApplicationRecord
   has_and_belongs_to_many :events
+  
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :bands
+  
+  validates :name, :venue, :location, :date, presence: true
+  validates :name, uniqueness: { scope: :date, message: "already exists for this date" }
 end
