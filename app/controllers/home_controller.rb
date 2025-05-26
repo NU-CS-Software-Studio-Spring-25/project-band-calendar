5c057c635ed33a92d253b85460229ea33827936c
Kevin Wang <kenwang3333@gmail.com>
2025-05-26 09:44:11 -0500

    Add home page with venues/events by date and comprehensive seed data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @date = params[:date].present? ? Date.parse(params[:date]) : Date.today
+    @venues = Venue.joins(:events).where("date(events.date) = ?", @date).order(:name).distinct
+    @events_by_venue = {}
+    
+    @venues.each do |venue|
+      @events_by_venue[venue.id] = venue.events
+                                        .where("date(date) = ?", @date)
+                                        .includes(band_events: :band)
+                                        .order(:date)
+    end
   end
 end 
