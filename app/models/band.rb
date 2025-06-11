5c75f34946a9b4e8a2a40ab63a23c68c319dc0a0
irving-peng <irvingcpeng@gmail.com>
2025-06-11 13:15:48 -0500

    full rdoc

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,23 @@
+# == Schema Information
+#
+# Table name: bands
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  photo_url  :string
+#  bio        :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+# Represents a musical band that can perform at multiple events.
+#
+# A band may have many scheduled performances (band_events) and be associated with many events.
 class Band < ApplicationRecord
+  # Associations
   has_many :band_events, dependent: :destroy
   has_many :events, through: :band_events
-  
+
+  # Validations
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
